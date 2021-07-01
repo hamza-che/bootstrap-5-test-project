066afe96de1e1964727143265b68e0883e5bf235
hamza-che <chenguitiha@gmail.com>
2021-07-01 21:12:12 +0100

    bootstrap 5 test project upload the complete project

---
+++ main.js
@@ -0,0 +1,6 @@
+mapboxgl.accessToken = 'pk.eyJ1IjoiaGFtemFjaDk3IiwiYSI6ImNrcWxiZWNlcjN6M3kyd212MWZseWF4ZXcifQ.xwVPIODcdaYqfYW4NvGhyw';
+var map = new mapboxgl.Map({
+	container: 'map',
+	style: 'mapbox://styles/mapbox/streets-v11'
+	// center: [33.796296512357024, 2.8619866526413844]
+});
